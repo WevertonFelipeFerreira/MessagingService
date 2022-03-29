fffbc27bfb9938b4bcf49986a73efd5a13ac5856
Weverton Ferreira <contato.wevertonfelipe@gmail.com>
2022-03-28 22:31:41 -0300

    Publish message implemented

---
+++ MessageService.Publisher/Controllers/MessageController.cs
@@ -0,0 +1,42 @@
+﻿using MessageService.Publisher.DTOs.Request;
+using MessageService.Publisher.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MessageService.Publisher.Controllers
+{
+    [ApiController]
+    [Route("message")]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMessagePublisher _messagePublisher;
+        public MessageController(IMessagePublisher messagePublisher)
+        {
+            _messagePublisher = messagePublisher;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request">Request contract to send message to the queue</param>
+        /// <returns></returns>
+        [HttpPost("send")]
+        public async Task<IActionResult> SendMessage([FromBody] SendMessageRequest request)
+        {
+            var seq = await _messagePublisher.Publish(request);
+            return Ok(seq);
+        }
+
+        /// <summary>
+        /// Cancel a message in the queue
+        /// </summary>
+        /// <param name="id">Message indentifier</param>
+        /// <returns></returns>
+        [HttpDelete("send/{id}")]
+        public async Task<IActionResult> DeleteMessage(long id)
+        {
+            await _messagePublisher.CancelMessage(id);
+            return NoContent();
+        }
+    }
+}
---
+++ MessageService.Publisher/Service/IMessagePublisher.cs
@@ -0,0 +1,11 @@
+﻿using MessageService.Publisher.DTOs.Request;
+using System.Threading.Tasks;
+
+namespace MessageService.Publisher.Service
+{
+    public interface IMessagePublisher
+    {
+        public Task<long> Publish(SendMessageRequest obj);
+        public Task CancelMessage(long sequence);
+    }
+}
